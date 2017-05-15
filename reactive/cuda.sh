df328e005337e28cd2281e5c0b14991cd9e87da2
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2017-05-15 20:50:58 +0300

    Cleaning up the code (#5)

--- reactive/cuda.sh
+++ reactive/cuda.sh
@@ -5,8 +5,6 @@ source charms.reactive.sh
 
 CUDA_VERSION=$(config-get cuda-version | awk 'BEGIN{FS="-"}{print $1}')
 CUDA_SUB_VERSION=$(config-get cuda-version | awk 'BEGIN{FS="-"}{print $2}')
-# CUDA_PKG_VERSION="7-5"
-NVIDIA_DRIVER_VERSION="375.26"
 SUPPORT_CUDA="$(lspci -nnk | grep -iA2 NVIDIA | wc -l)"
 ROOT_URL="http://developer.download.nvidia.com/compute/cuda/repos"
 
@@ -28,8 +26,6 @@ case "$(arch)" in
     ;;
     "ppc64le" | "ppc64el" )
         ARCH="ppc64le"
-        # The checksum of the gdk installer.
-        NVIDIA_GDK_INSTALLER_SUM="064678e29d39f0c21f4b66c5e2fb18ba65fd9bc3372d0b319c31cab0e791fc1c"
     ;;
     * )
         juju-log "Your architecture is not supported. Exiting"
@@ -51,108 +47,6 @@ case "${UBUNTU_CODENAME}" in
         exit 1
     ;;
 esac
-
-#####################################################################
-#
-# Install nvidia GDK per architecture
-#
-#####################################################################
-
-function all:all:install_nvidia_gdk() {
-    NVIDIA_GDK_INSTALL_PATH="/opt/nvidia-gdk"
-    NVIDIA_GDK_CONF_FILE="/etc/ld.so.conf.d/nvidia-gdk.conf"
-    NVIDIA_GDK_INSTALLER_URL="http://developer.download.nvidia.com/compute/cuda/${CUDA_VERSION}/Prod/gdk/gdk_linux_${ARCH}_352_79_release.run"
-
-    if [ ! -d "${NVIDIA_GDK_INSTALL_PATH}" ]; then
-      mkdir -p "${NVIDIA_GDK_INSTALL_PATH}"
-    fi
-    if [ ! -d "${NVIDIA_GDK_INSTALL_PATH}" ] ||
-       [ ! -w "${NVIDIA_GDK_INSTALL_PATH}" ]; then
-      juju-log "Error: Inaccessible path: ${NVIDIA_GDK_INSTALL_PATH}"
-      juju-log "       The installation directory is either inaccessable or"
-      juju-log "       cannot be created because you do not have the proper"
-      juju-log "       permissions to access it. Please select a different"
-      juju-log "       location, or rerun this command as a user with the"
-      juju-log "       proper permissions, e.g. using ''."
-      exit 1;
-    fi
-
-    # Create a place for storing temporary files and make sure this
-    # directory gets deleted whenever this script exits.
-    NVIDIA_TEMP_DIR=$(mktemp -d)
-    trap "rm -rf ${NVIDIA_TEMP_DIR}" EXIT
-
-    # Download and run the Nvidia GDK installer.
-    NVIDIA_GDK_INSTALLER_FILE="${NVIDIA_TEMP_DIR}/gdk"
-
-    wget -O ${NVIDIA_GDK_INSTALLER_FILE} -q ${NVIDIA_GDK_INSTALLER_URL}
-    RESULT="${?}"
-
-    if [ ${RESULT} != "0" ]; then
-      juju-log "Failed to download the installer!"
-      juju-log "Error running wget, please check your internet connection:"
-      juju-log "  \$ wget ${NVIDIA_GDK_INSTALLER_URL}"
-      exit ${RESULT}
-    fi
-
-    juju-log "${NVIDIA_GDK_INSTALLER_SUM}  ${NVIDIA_GDK_INSTALLER_FILE}" \
-        | sha256sum -c --strict -
-    RESULT="${?}"
-
-    if [ ${RESULT} != "0" ]; then
-      juju-log "Failed verifying the checksum of the downloaded Nvidia GDK installer!"
-      juju-log "Please report this error to dev@mesos.apache.org."
-      exit ${RESULT}
-    fi
-
-    # Remove a symbolic link so that running this script is idempotent.
-    # Without removing this symbolic link, the GDK installer script errors
-    # out with the line:
-    #   'ln: failed to create symbolic link <path_to_file>: File exists'
-    # Since we know the next thing we will do is run the installer,
-    # manually removing this file should be harmless.
-    rm -rf ${NVIDIA_GDK_INSTALL_PATH}/usr/bin/nvvs
-
-    # Run the installer.
-    # We pass the '--silent' flag here to make the GDK installer happy.
-    # Without it, it errors out with the line:
-    #  'The installer must be run in silent mode to use command-line options.'
-    chmod +x "${NVIDIA_GDK_INSTALLER_FILE}"
-    ${NVIDIA_GDK_INSTALLER_FILE} --installdir="${NVIDIA_GDK_INSTALL_PATH}" --silent
-
-    # Optionally update the ld cache with the Nvidia GDK library path.
-    cat > ${NVIDIA_GDK_CONF_FILE} << EOF
-# nvidia-gdk default configuration
-${NVIDIA_GDK_INSTALL_PATH}/usr/src/gdk/nvml/lib/
-EOF
-
-    ldconfig
-    juju-log "Wrote '${NVIDIA_GDK_CONF_FILE}' and ran ldconfig"
-}
-
-function trusty::x86_64::install_nvidia_gdk() {
-    # The checksum of the gdk installer.
-    NVIDIA_GDK_INSTALLER_SUM="3fa9d17cd57119d82d4088e5cfbfcad960f12e3384e3e1a7566aeb2441e54ce4"
-    all::all:install_nvidia_gdk
-}
-
-function xenial::x86_64::install_nvidia_gdk() {
-    # The checksum of the gdk installer.
-    NVIDIA_GDK_INSTALLER_SUM="3fa9d17cd57119d82d4088e5cfbfcad960f12e3384e3e1a7566aeb2441e54ce4"
-    all::all:install_nvidia_gdk
-}
-
-function trusty::ppc64le::install_nvidia_gdk() {
-    # The checksum of the gdk installer.
-    NVIDIA_GDK_INSTALLER_SUM="064678e29d39f0c21f4b66c5e2fb18ba65fd9bc3372d0b319c31cab0e791fc1c"
-    all::all:install_nvidia_gdk
-}
-
-function xenial::ppc64le::install_nvidia_gdk() {
-    # The checksum of the gdk installer.
-    NVIDIA_GDK_INSTALLER_SUM="064678e29d39f0c21f4b66c5e2fb18ba65fd9bc3372d0b319c31cab0e791fc1c"
-    all::all:install_nvidia_gdk
-}
 
 #####################################################################
 #
@@ -178,10 +72,6 @@ function xenial::x86_64::install_nvidia_driver() {
 }
 
 function trusty::ppc64le::install_nvidia_driver() {
-    # wget -c http://us.download.nvidia.com/Ubuntu/"${NVIDIA_DRIVER_VERSION}"/NVIDIA-Linux-"${ARCH}"-"${NVIDIA_DRIVER_VERSION}".run -P /tmp
-    # chmod +x /tmp/NVIDIA-Linux-"${ARCH}"-"${NVIDIA_DRIVER_VERSION}".run
-    # /tmp/NVIDIA-Linux-"${ARCH}"-"${NVIDIA_DRIVER_VERSION}".run -a -q -s --disable-nouveau
-    # all:all:install_nvidia_driver
     bash::lib::log warn "This task is handled by the cuda installer"
 }
 
@@ -242,13 +132,6 @@ function all::x86_64::install_cuda() {
 }
 
 function trusty::x86_64::install_cuda() {
-    # NVIDIA_GPGKEY_SUM="bd841d59a27a406e513db7d405550894188a4c1cd96bf8aa4f82f1b39e0b5c1c"
-    # NVIDIA_GPGKEY_FPR="889bee522da690103c4b085ed88c3d385c37d3be"
-
-    # apt-key adv --fetch-keys ${ROOT_URL}/GPGKEY && \
-    #     apt-key adv --export --no-emit-version -a $NVIDIA_GPGKEY_FPR | tail -n +2 > cudasign.pub && \
-    #     echo "$NVIDIA_GPGKEY_SUM cudasign.pub" | sha256sum -c --strict - && rm cudasign.pub && \
-    #     echo "deb ${ROOT_URL}/ubuntu1404/x86_64 /" > /etc/apt/sources.list.d/cuda.list
     all::x86_64::install_cuda
 }
 
@@ -257,13 +140,6 @@ function xenial::x86_64::install_cuda() {
 }
 
 function trusty::ppc64le::install_cuda() {
-    # wget -c -P /tmp "http://developer.download.nvidia.com/compute/cuda/${CUDA_VERSION}/Prod/local_installers/cuda-repo-ubuntu1404-${CUDA_PKG_VERSION}-local_${CUDA_VERSION}-${CUDA_SUB_VERSION}_ppc64el.deb"
-    # dpkg -i /tmp/cuda-repo-ubuntu1404-${CUDA_PKG_VERSION}-local_${CUDA_VERSION}-${CUDA_SUB_VERSION}_ppc64el.deb
-
-    # apt-add-repository -y ppa:openjdk-r/ppa
-    # apt-add-repository -y ppa:jochenkemnade/openjdk-8
-
-    # all::all::install_cuda
     bash::lib::die This OS is not supported by nVidia for CUDA 8.0. Please upgrade to 16.04
 }
 
@@ -350,14 +226,6 @@ function install_cuda() {
         build-essential
 
     status-set maintenance "Installing CUDA"
-
-    # The gdk could be installed regardless of whether a gpu is detected, and
-    # used to compile CUDA apps on this host, even if they can't be run.
-    # For now we'll skip this to shorten install time, but we could make this
-    # possible later with a config option or something.
-    #
-    # ${UBUNTU_CODENAME}::${ARCH}::install_nvidia_gdk
-
 
     # This is a hack as for some reason this package fails
     dpkg --remove --force-remove-reinstreq grub-ieee1275 || juju-log "not installed yet, forcing not to install"
